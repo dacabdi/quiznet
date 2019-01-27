1fa598dbb860062a89851e9a3b4406902c2a4c1e
David Cabrera <dacabdi89@ufl.edu>
2019-01-27 17:17:16 -0500

    building server layout

--- src/server.cpp
+++ src/server.cpp
@@ -1,11 +1,39 @@
-#include <iostream>
-#include "header.h"
+#include <stdlib.h>     // exit, etc
+#include <stdio.h>      // in/out streams
+#include <sys/types.h>  // data types for syscalls
+#include <sys/socket.h> // structs for sockets
+#include <netinet/in.h> // constants and structs for internet domain addresses
 
-using namespace std;
-
-int main(void)
+// handle and display error
+void error(char *msg)
 {
-    cout << "test" << endl;
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+/* 
+    server setup:
+
+        1. create a socket using the socket syscall
+        2. bind the socket using the bind syscall
+        3. listen for connections using the listen syscall
+        4. accept a connection with the accept syscall, this is a blocking call
+        5. rx/tx data
+*/
+
+int main(int argc, char* argv[])
+{
+    int sockfd,     // file descriptor for socket sys call
+        newsockfd,  // file descriptor for the accept sys call
+        portno,     // port number accepting connections
+        clilen,     // client address size
+        n;          // read/write return
+
+    
+    
+
+    
+
 
     return 0;
 }
