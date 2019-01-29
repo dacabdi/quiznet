cb9716aee825ababb0173af24e2c3486b4aeda1f
David Cabrera <dacabdi89@ufl.edu>
2019-01-28 20:33:27 -0500

    server class skeleton

--- inc/Server.h
+++ inc/Server.h
@@ -1,19 +1,59 @@
 #ifndef __SERVER__H__
 #define __SERVER__H__
 
+#define DEFAULT_PORT "8080"
+#define ERR_BUFFER_LENGTH 256
+
+#ifdef VERBOSE
+    #include <iostream>
+#endif
+
 #include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+#include <errno.h>  // C error numbers for sys libraries
+#include <string.h> // strerror_r
+#include <unistd.h>
+//#include <stdlib.h>
+
+#include <string>
+#include <stdexcept>
+
+struct socketSettings 
+{
+    int addressDomain = AF_INET;
+    int socketType = SOCK_STREAM;
+    int protocol = 0; // let the OS choose
+};
 
 class Server                                                                  
 {                                                                                
     private:                                              
 
-        Server(void);                                                             
-        static Server * _server;
-        static bool _initFlag;
-                                                                                 
+        const u_int port;
+        
+        int socketFd;
+        struct socketSettings socketSettings;
+        struct sockaddr_in address;
+
+        bool isListening = false;
+        uint backlogLength = 10; // arbitrary
+
+        char errBuffer[ERR_BUFFER_LENGTH];
+
+        int createSocket(void);
+        struct sockaddr_in bindSocket(void);
+        void startListening();
+
+        inline void logTerm(const std::string& str);
+
+        void error(void);
+
     public:                            
 
-        static Server& getServer(void);                                   
+        Server(const std::string userPort = DEFAULT_PORT);
 };                                                                               
 
 #endif // __SERVER__H__
--- src/Server.cpp
+++ src/Server.cpp
@@ -1,20 +1,65 @@
 #include "Server.h"
 
-Server * Server::_server = nullptr;
-bool Server::_initFlag = false;
-
-Server::Server(void)
+Server::Server(const std::string userPort) : port(std::stoul(userPort))
 {
-    _initFlag = true;
+    // 1. CREATE SOCKET
+    socketFd = createSocket();
+    // 2. BIND SOCKET
+    address = bindSocket();
+    // 3. START LISTENING
+    startListening();
 }
 
-Server& Server::getServer(void)                                              
-{                                                                                
-    if (!_initFlag)
-    {
-        _server = new Server();
-        return *(_server);
-    }
+int Server::createSocket(void)
+{
+    logTerm("Creating socket... ");
+
+    int _socketFd = socket(
+        socketSettings.addressDomain, 
+        socketSettings.socketType, 
+        socketSettings.protocol
+    );
     
-    return *(_server);
+    if (_socketFd < 0)
+    {
+        
+    }
+
+    logTerm("[OK]\n");
+}
+
+struct sockaddr_in Server::bindSocket()
+{
+    logTerm("Binding socket... ");
+
+    bzero((char *) &address, sizeof(address)); // initialize buff to 0
+    address.sin_family = socketSettings.addressDomain; // address domain
+    address.sin_port = htons(port);            // conv to net byte order
+    address.sin_addr.s_addr = INADDR_ANY;      // any incoming
+    
+    if (bind(socketFd, (struct sockaddr*)&address, sizeof(address)) < 0)
+        error();
+        
+    logTerm("[OK]\n");
+}
+
+void Server::startListening(void)
+{
+    logTerm("Listening to connections... ");
+    listen(socketFd, backlogLength);
+    logTerm("[OK]\n");
+}
+
+void Server::error(void)
+{
+    strerror_r(errno, errBuffer, ERR_BUFFER_LENGTH);
+    close(socketFd);
+    throw std::runtime_error(errBuffer);
+}
+
+inline void logTerm(const std::string& str)
+{
+    #ifdef VERBOSE
+        std::cout << str << std::flush;
+    #endif
 }
