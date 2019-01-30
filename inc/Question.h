0ecb578c0945d0a86f564c5047b070cdb947232b
dacabdi <dacabdi89@ufl.edu>
2019-01-30 07:22:33 -0500

    starting quizbook

---
+++ inc/Question.h
@@ -0,0 +1,15 @@
+#ifndef __QUESTION__H__
+#define __QUESTION__H__
+
+class Question
+{
+    serialize()
+    
+    id
+    tag
+    question
+    choices {map (character : string)}
+};
+
+
+#endif // __QUESTION__H__
---
+++ inc/Quizbook.h
@@ -0,0 +1,19 @@
+#ifndef __QUIZBOOK__H__
+#define __QUIZBOOK__H__
+
+#include "Question.h"
+
+class Quizbook
+{
+    saveToFile()
+    loadFromFile()
+    serialize()
+    
+    
+
+    map of Question objets
+        (id) : (question)
+};
+
+
+#endif // __QUIZBOOK__H__
